9105e9374bcaf0bfce630ab2edef4d730efcd235
rraagg <rob.a.gallagher@gmail.com>
2025-06-08 22:44:24 -0700

    added convert int to string

--- practice/stringConvert.go
+++ practice/stringConvert.go
@@ -7,8 +7,10 @@ import (
 
 func StringToInt() {
 	n, err := strconv.Atoi("345")
+	k := strconv.Itoa(345)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Printf("%v = %T\n", n, n)
+	fmt.Printf("%v = %T\n", k, k)
 }
